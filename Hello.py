82128356c88f4476c636be14e3854584f217b434
kamel589 <kamel.sehaki@gmail.com>
2021-12-10 11:09:47 +0100

    Ajout nouveau fichier

---
+++ Hello.py
@@ -0,0 +1,8 @@
+#! /usr/local/bin/python3
+print('Hello, Data World!')
+print('Hello, Data Architect 2021!')
+print ('Hello, Data Architect 2022 !')
+print('Hello, Data Analyst 2021!')
+print ('Hello, Data Analyst 2022 !')
+
+
